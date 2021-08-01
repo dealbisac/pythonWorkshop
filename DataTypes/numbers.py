3492bb67558639398d39895decbb81b411346d32
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2021-08-01 12:31:45 +0545

    number types added

---
+++ DataTypes/numbers.py
@@ -0,0 +1,15 @@
+a = 5+6
+print(a)
+print(type(a))
+
+# complex
+c = 5+3j
+
+print(c+3)
+print(type(c))
+
+print(isinstance(c, complex))
+
+# Binary - 0b, 0B | Ocatl 0o , 0O | Hex 0x, 0X
+
+print(0b1101011)
